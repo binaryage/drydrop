1192d422a6f1af2ada1b1d9894812a1bba026e01
Antonin Hildebrand <antonin@hildebrand.cz>
2008-12-13 21:20:17 +0100

    fixed bug with email parameter

--- dryapp/drydrop/app/core/events.py
+++ dryapp/drydrop/app/core/events.py
@@ -2,8 +2,8 @@ import logging
 from drydrop.app.models import Event
 from google.appengine.api import users
 
-def log_event(action, code = 0, user = None):
-    if user is None:
+def log_event(action, code = 0, email = None):
+    if email is None:
         user = users.get_current_user()
         if user is None:
             email = None
